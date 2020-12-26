7e6c3a838452c605807739c83e7332379719da3f
KF <Thitipong.poo@ku.th>
2020-12-26 18:52:32 +0700

    initial commit

---
+++ src/main/java/th/ac/ku/atm/ATMConfig.java
@@ -0,0 +1,26 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource(){
+        return new DataSourceFile("customers.txt");
+    }
+
+    @Bean
+    public Bank bank(){
+        return new Bank("Fang Bank", dataSource());
+    }
+
+    @Bean ATM atm(){
+        return new ATM(bank());
+    }
+
+    @Bean AtmUI atmUI(){
+        return new AtmUI(atm());
+    }
+
+}
